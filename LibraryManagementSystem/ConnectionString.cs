c72594f6a50244e9dc79d9bed7e1a2dd3102705f
Thant Zaw Htet <Thant.Zaw.Htet@fusionsol.com>
2025-01-24 23:12:39 +0630

    Add project files.

---
+++ LibraryManagementSystem/ConnectionString.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem
+{
+    public class ConnectionString
+    {
+        public static string getConnection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Documents\library.mdf;Integrated Security=True;Connect Timeout=30";
+
+    }
+}
